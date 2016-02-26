28c899c56713e6fd51a04bdfb8c71a8247462b9d
yuzu <snowboard.441@gmail.com>
2016-02-26 23:29:24 +0900

    animation設定

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 (function() {
   var chocoBox = document.querySelector('.choco-box');
   chocoBox.addEventListener('click', function(e) {
+    chocoBox.classList.add('hide-ani');
+  });
+
+  chocoBox.addEventListener('webkitAnimationEnd', function(e) {
     chocoBox.classList.add('hide');
   });
 })();
